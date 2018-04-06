fa35df097998b6d4db6f5f4cefe0f26cd5bf6fed
Andreas Kling <a.kling@dkfz-heidelberg.de>
2018-04-06 14:26:01 +0200

    Add equals() to BufferValue

--- src/main/groovy/de/dkfz/roddy/tools/BufferValue.groovy
+++ src/main/groovy/de/dkfz/roddy/tools/BufferValue.groovy
@@ -79,4 +79,14 @@ public class BufferValue {
             return alignedValue
         return alignedValue * baseUnit.multiplier / unit.multiplier as Long
     }
+
+    @Override
+    boolean equals(Object other) {
+        if (other instanceof BufferValue &&
+                other.alignedValue == this.alignedValue &&
+                other.baseUnit == this.baseUnit) {
+            return true
+        }
+        return false
+    }
 }
